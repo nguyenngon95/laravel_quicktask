7de0b1b989b5f47d4165bbcaca47da764155a04a
Ngon Nguyen <nguyenngon95@gmail.com>
2018-10-24 14:00:21 +0700

    Edit and Delete

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -34,6 +34,7 @@
                     <thead>
                         <th>@lang('messages.task')</th>
                         <th>&nbsp;</th>
+                        <th>&nbsp;</th>
                     </thead>
 
                     <tbody>
@@ -44,6 +45,17 @@
                                 </td>
 
                                 <td>
+                                    {!! Form::open(['route' => ['tasks.edit', $task->id], 'method' => 'get']) !!}
+
+                                        {{ Form::submit(trans('messages.edit'), ['class' => 'btn btn-warning', 'id' => 'edit-task-'.$task->id]) }}
+                                    {!! Form::close() !!}
+                                </td>
+
+                                <td>
+                                    {!! Form::open(['url' => 'tasks/'.$task->id, 'method' => 'delete']) !!}
+
+                                        {{ Form::submit(trans('messages.delete'), ['class' => 'btn btn-danger', 'id' => 'delete-task-'.$task->id]) }}
+                                    {!! Form::close() !!}
                                 </td>
                             </tr>
                         @endforeach
